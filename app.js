503148e48fef7c185b19bca6c849e27284c77fe7
KBCodes2021 <85765237+KBCodes2021@users.noreply.github.com>
2021-12-13 16:05:12 -0800

    bug fixes and new projects added

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-
-getElementById("root");
-
-
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000");
 });
